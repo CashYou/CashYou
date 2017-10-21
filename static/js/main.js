74cfb9fc4badb19b41b7f7d1356fa6f14f78cc17
Yichen Jiang <pauljyc0929@gmail.com>
2017-10-21 10:49:04 -0400

    add login

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,14 @@ window.fbAsyncInit = function() {
   FB.AppEvents.logPageView();
 };
 
+var finished_rendering = function() {
+console.log("finished rendering plugins");
+var spinner = document.getElementById("spinner");
+spinner.removeAttribute("style");
+spinner.removeChild(spinner.childNodes[0]);
+}
+FB.Event.subscribe('xfbml.render', finished_rendering);
+
 (function(d, s, id){
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
@@ -16,40 +24,39 @@ window.fbAsyncInit = function() {
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
 
-// initialize Account Kit with CSRF protection
-AccountKit_OnInteractive = function(){
-   AccountKit.init(
-     {
-       appId:124172418273424,
-       state:0929,
-       version:"{{ACCOUNT_KIT_API_VERSION}}",
-       fbAppEventsEnabled:true,
-       redirect:"https://www.google.com"
-     }
-   );
-};
-
-// login callback
-function loginCallback(response) {
-   if (response.status === "PARTIALLY_AUTHENTICATED") {
-     var code = response.code;
-     var csrf = response.state;
-     // Send code to server to exchange for access token
-   }
-   else if (response.status === "NOT_AUTHENTICATED") {
-     // handle authentication failure
-   }
-   else if (response.status === "BAD_PARAMS") {
-     // handle bad parameters
-   }
-}
-
-// email form submission handler
-function emailLogin() {
-   var emailAddress = document.getElementById("email").value;
-      AccountKit.login(
-        'EMAIL',
-        {emailAddress: emailAddress},
-        loginCallback
-      );
-}
+// // initialize Account Kit with CSRF protection
+// AccountKit_OnInteractive = function(){
+//    AccountKit.init(
+//      {
+//        appId:124172418273424,
+//        state:0929,
+//        version:"{{ACCOUNT_KIT_API_VERSION}}",
+//        fbAppEventsEnabled:true,
+//        redirect:"https://www.google.com"
+//      }
+//    );
+// };
+//
+// // login callback
+// function loginCallback(response) {
+//    if (response.status === "PARTIALLY_AUTHENTICATED") {
+//      var code = response.code;
+//      var csrf = response.state;
+//      // Send code to server to exchange for access token
+//    }
+//    else if (response.status === "NOT_AUTHENTICATED") {
+//      // handle authentication failure
+//    }
+//    else if (response.status === "BAD_PARAMS") {
+//      // handle bad parameters
+//    }
+// }
+//
+// // email form submission handler
+// function emailLogin() {
+//    var emailAddress = document.getElementById("email").value;
+//       AccountKit.login(
+//         'EMAIL',
+//         loginCallback
+//       );
+// }

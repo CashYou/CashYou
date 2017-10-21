3fd94c7fecb8a37b918b9237c1248249786f921f
Yichen Jiang <pauljyc0929@gmail.com>
2017-10-21 09:26:54 -0400

    new login

--- static/js/main.js
+++ static/js/main.js
@@ -16,31 +16,15 @@ window.fbAsyncInit = function() {
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
 
- (function(d, s, id) {
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) return;
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
-(function(d, s, id) {
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) return;
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-
 // initialize Account Kit with CSRF protection
 AccountKit_OnInteractive = function(){
    AccountKit.init(
      {
        appId:124172418273424,
-       state:"{{csrf}}",
+       state:0929,
        version:"{{ACCOUNT_KIT_API_VERSION}}",
        fbAppEventsEnabled:true,
-       redirect:"{{REDIRECT_URL}}"
+       redirect:"https://www.google.com"
      }
    );
 };
@@ -58,17 +42,6 @@ function loginCallback(response) {
    else if (response.status === "BAD_PARAMS") {
      // handle bad parameters
    }
-}
-
-// phone form submission handler
-function smsLogin() {
-   var countryCode = document.getElementById("country_code").value;
-   var phoneNumber = document.getElementById("phone_number").value;
-   AccountKit.login(
-     'PHONE',
-     {countryCode: countryCode, phoneNumber: phoneNumber}, // will use default values if not specified
-     loginCallback
-   );
 }
 
 // email form submission handler

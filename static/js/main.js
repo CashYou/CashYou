3732499092274413c87fb755d870874df4d46b02
Vicky McDermott <vickymmcd@gmail.com>
2017-10-21 08:03:07 -0400

    working on js

--- static/js/main.js
+++ static/js/main.js
@@ -8,6 +8,7 @@ window.fbAsyncInit = function() {
   FB.AppEvents.logPageView();
 };
 
+$(document).ready(function(){
 (function(d, s, id){
    var js, fjs = d.getElementsByTagName(s)[0];
    if (d.getElementById(id)) {return;}
@@ -39,3 +40,5 @@ window.fbAsyncInit = function() {
    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
    fjs.parentNode.insertBefore(js, fjs);
  }(document, 'script', 'facebook-jssdk'));
+
+});

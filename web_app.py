2aeaba46c2812ba94a4248a58fd2871825db593d
Vicky McDermott <vickymmcd@gmail.com>
2017-10-22 06:51:24 -0400

    getting things to work

--- web_app.py
+++ web_app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # change lager
-    return render_template('elate/portal.html')
+    return render_template('elate/index.html')
 
 @app.route('/portal/')
 def portal():
@@ -66,7 +66,7 @@ def result():
                          error=error, script=script, div=div)
 
 if __name__ == '__main__':
-    #HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-    #PORT = int(os.environ.get('PORT', 5000))
-    #app.run(host=HOST, port=PORT)
-    app.run()
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host=HOST, port=PORT)
+    #app.run()
